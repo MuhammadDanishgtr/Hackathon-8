0d87a8862e3b5ad74aa5c7b50e050eb78faede26
MuhammadDanishgtr <162627889+MuhammadDanishgtr@users.noreply.github.com>
2025-02-04 21:46:18 +0500

    Successfully Order Fatch into Sanity Studio

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -44,6 +44,7 @@ export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '6dx88lag';
+export const token = process.env.SANITY_AUTH_TOKEN || 'skxkO0KEGSPzt6PMkUZttWqfxA4SbtR6tXWhvH8LBSKtL98K1zKf47hnwKV6NunlCL6aAqLjFdq5TOqh1Ig2dlviFFO94qMsyGfKm1fhrQd4hrOkkZcBSB6TDux61nUWurWr8XxFgn9FBhd1xR99dynSic3FRR8KXW43rer4C8UMUBxZz1hn';
 
 export function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,9 +2,10 @@ import { type SchemaTypeDefinition } from 'sanity'
 import {productSchema}  from './products'
 // import {categorySchema} from ./Categories
 import {categorySchema} from './categories'
+import order from './order'
 
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [productSchema, categorySchema  ],
+  types: [productSchema, categorySchema, order],
 }
